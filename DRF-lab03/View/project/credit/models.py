69f791da640a9bea533f7c22dcbe316bec52e41a
PHHHung <phihung06022002@gmail.com>
2024-10-02 12:35:36 +0700

    lab02

--- DRF-lab03/View/project/credit/models.py
+++
@@ -1,9 +0,0 @@
-# credit/models.py
-from django.db import models
-
-class Report(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
--- DRF-lab03/View/project/polls/views.py
+++
@@ -1,7 +0,0 @@
-# polls/views.py
-from django.shortcuts import redirect
-from django.urls import reverse
-
-def some_view(request):
-    url = reverse('author-polls:index', current_app='author-polls')
-    return redirect(url)
